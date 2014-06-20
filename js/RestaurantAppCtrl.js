36092f1b46681d17489119613617488c89666a4d
zirbem07 <maxwell.zirbel@gmail.com>
2014-06-19 23:44:27 -0700

    Accordion Fixed

--- js/RestaurantAppCtrl.js
+++ js/RestaurantAppCtrl.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-    var total = 0;
-
+    //when user clicks a restaurant, display the appropriate menu
     $(".restaurant").click(function () {
         $(".menu").hide();
         $("#menuItems" + $(this).attr("id")).fadeIn("slow");
@@ -15,37 +14,40 @@ $(document).ready(function() {
 
     });
 
-    $(function () {
-        $("#accordion").accordion();
-    });
 
-
-
+    //When the user clicks the submit button attempt to submit order
     $(".btn").click(function (e) {
-        total = 0;
+        //initialize order total to zero
+        var total = 0;
 
+        //find all items and their value that the user checked
         var selected = $("#checkboxes input:checked").map(function(index, element) {
             total += parseInt($(this).attr("data-price") ,10);
             return element.name;
         }).get();
 
+        //make sure the user selected at least one order
         if(selected != "") {
             selected = selected.join(",");
-            //un email phone contactMethod restaurantName order
+
+            //check to see if the user entered a username
             var un = $('#un').val();
             if (un == "") {
                 alert("error You must enter a user name");
             } else {
+                //valid order, store the data
                 var restaurant = $('#nameDiv').html();
                 var date = new Date().toDateString();
                 var data = un + ":" + restaurant + ":" + date + ":" + total + ":" + selected;
                 var key = new Date();
                 storeData(key, data);
 
+                //uncheck all check boxes for the next order
                 $('input[type=checkbox]').each(function () {
                     $(this).prop("checked", false);
                 });
 
+                //empty the username field
                 $('#un').val("");
 
             }
@@ -55,6 +57,7 @@ $(document).ready(function() {
 
     });
 
+    //store the data in Firebase
     var storeData = function(key, data)
     {
         //Data Format: User Name : Restaurant Name : Date : Item1,Item2,Ect...
@@ -62,27 +65,34 @@ $(document).ready(function() {
         myDataRef.push({data: data});
         myDataRef.on('child_added', function(order) {
             var items = order.val();
-            displayOrder(items.data);
+            displayOrderHistory(items.data);
         });
-        localStorage.setItem(key, data);
+        //localStorage.setItem(key, data);
     }
-   /* function displayOrder(data) {
+
+    function displayOrderHistory(data) {
         var arr = data.split(":")
         var name = arr[0];
         var rest = arr[1];
         var date = arr[2];
         var total = arr[3];
-        var title = "<h3><strong>Name:</strong> " + name +"&nbsp<strong>Restaurant:</strong> "+ rest +"<strong>Date:</strong>  "+ date + " </h3>";
+        var title = "<h3><strong>Name:</strong> " + name +"<strong>Restaurant:</strong> "+ rest +"<strong>Date:</strong>  "+ date + " </h3>";
         var arr2 = arr[4].split(",");
-        var items = "<div><ul>";
+        var items = "<div>";
         for(var index in arr2){
-            items += "<li>" + arr2[index] + "</li><br />";
+            items += "<p>" + arr2[index] + "</p>";
         }
-        items += "</ul><span class='pull-right'><strong>Total: </strong> $"+total+ " </span></div>";
+        items += "<span class='pull-right'><strong>Total: </strong> $"+total+ " </span></div>";
         var content = title + items;
-        $('#accordion').append(content)
-    }*/
-    for(var i in localStorage)
+        $('#accordion').append(content);
+
+        //jQuery default accordion function
+        $(function () {
+            $("#accordion").accordion();
+        });
+    }
+
+    /*for(var i in localStorage)
     {
         var arr = localStorage[i].split(":");
         var name = arr[0];
@@ -97,7 +107,7 @@ $(document).ready(function() {
         }
         items += "</ul><span class='pull-right'><strong>Total: </strong> $"+total+ " </span></div>";
         var content = title + items;
-        $('#accordion').append(content)
-    }
+        $('#accordion').append(content);
+    }*/
 
 });
